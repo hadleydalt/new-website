5920a7856f496a82d1475880e171f9154996da00
hadleydalt <hadley_dalton@brown.edu>
2022-06-14 22:08:29 +0100

    animated topbar links working

--- src/splashpage/TopBarLink.js
+++ src/splashpage/TopBarLink.js
@@ -5,8 +5,26 @@ import {useSpring, animated} from 'react-spring'
 export const TopBarLink = (props) => {
     const [hovered, setHovered] = useState(false)
 
+    const style = useSpring({
+        lineHeight: 4.5,
+        fontFamily: 'Trebuchet MS',
+        textTransform: 'uppercase',
+        color: 'white',
+        fontWeight: 'bold',
+        filter: 'drop-shadow(0px 4px 4px rgba(0,0,0,0.25))',
+        paddingRight: 10,
+        paddingLeft: 10,
+        height: 67,
+        borderBottom: hovered ? '3px solid white' : '3px solid transparent', 
+        config: {
+            tension: 30,
+            friction: 5,
+        },
+    })
+
     const decStyle = useSpring({
-        lineHeight: hovered ? 45 : 30,
+        lineHeight: hovered ? 2.5 : 1.9,
+        fontSize: hovered ? 18 : 15,
         fontFamily: 'Trebuchet MS',
         textTransform: 'uppercase',
         color: 'white',
@@ -16,8 +34,8 @@ export const TopBarLink = (props) => {
         filter: 'drop-shadow(0px 4px 4px rgba(0,0,0,0.25))',
         paddingRight: hovered ? 20 : 10,
         paddingLeft: hovered ? 20 : 10,
-        height: hovered ? 45: 30,
-        borderRadius: hovered ? '2em 1em 4em / 0.5em 3em' : 25,
+        height: hovered ? 45 : 30,
+        borderRadius: hovered ? 0 : 25,
         border: '2px solid white', 
         config: {
             tension: 300,
@@ -29,7 +47,7 @@ export const TopBarLink = (props) => {
         if (!hovered) {
             return
         }
-    }, [hovered, 150])
+    }, [hovered, 100])
 
 
     if (props.dec) {
@@ -44,11 +62,11 @@ export const TopBarLink = (props) => {
         )
     } else {
         return (
-            <div 
+            <animated.div 
                 onMouseEnter={() => setHovered(true)} 
                 onMouseLeave={() => setHovered(false)}
-                className={hovered ? "topbar-link-hovered" : "topbar-link"}>{props.text}
-            </div>
+                style={style}>{props.text}
+            </animated.div>
         )
     }
 }
