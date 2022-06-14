f26d06cf0ef06414f0c45a93399a5908725d52ab
hadleydalt <hadley_dalton@brown.edu>
2022-06-14 22:28:52 +0100

    intro box animation

--- src/splashpage/SkyBackground.js
+++ src/splashpage/SkyBackground.js
@@ -1,16 +1,42 @@
 import '../App.css';
 import { Constellations } from './Constellations';
 import { TopBar } from './TopBar';
+import {useSpring, animated} from 'react-spring'
+import React, { useEffect, useState } from 'react';
+
 
 export const SkyBackground = () => {
+
+    const [hovered, setHovered] = useState(false)
+
+    const style = useSpring({
+        position: 'absolute', 
+        transform: hovered ? 'translate(0px, -20px)' : 'translate(0px, 0px)',
+        config: {
+            tension: 100,
+            friction: 10,
+        },
+    })
+
+    useEffect(() => {
+        if (!hovered) {
+            return
+        }
+    }, [hovered, 100])
+
     return (
         <>
             <div className="sky-background">
                 <div className="constellations-wrapper">
                     <Constellations />
-                    <div className="intro">
+                    <animated.div 
+                        className="intro" 
+                        style={style}
+                        onMouseEnter={() => setHovered(true)} 
+                        onMouseLeave={() => setHovered(false)}
+                    >
                         <img src={require('../graphics/TRANSPARENT SPLASH GRAPHIC.png')} />
-                    </div>
+                    </animated.div>
                 </div>
                 <TopBar />
             </div>
