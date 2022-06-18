c3a123137fc9b22f274e21bda7687accebe42323
hadleydalt <hadley_dalton@brown.edu>
2022-06-18 16:49:56 +0200

    not working because of hooks in class component

--- src/splashpage/Constellations.js
+++ src/splashpage/Constellations.js
@@ -1,20 +1,9 @@
 import '../App.css';
-import { animated } from 'react-spring'
-import React, { useEffect, useState } from 'react';
 
 export const Constellations = (props) => {
-    const [rotatePos, setRotatePos] = useState(props.rotatePos)
 
     const style = {
-        transform: `rotate(${rotatePos}deg)`
-    }
-
-    const handleScroll = () => {
-        if (window.scrollY > 360) {
-            setRotatePos(window.scrollY % 360)
-        } else {
-            setRotatePos(window.scrollY)
-        }
+        transform: `rotate(${props.rotatePos}deg)`
     }
 
     return (
--- src/splashpage/FrontPage.js
+++ src/splashpage/FrontPage.js
@@ -2,40 +2,67 @@ import '../App.css';
 import { Constellations } from './Constellations';
 import { TopBar } from './TopBar';
 import { useSpring, animated } from 'react-spring'
-import React, { useEffect, useState } from 'react';
-import { ArrowDown } from '../graphics/ArrowDown'
+import React, { useEffect } from 'react';
+import { ArrowDown } from '../graphics/ArrowDown';
+import { ExperimentalObj } from '../body/ExperimentalObj'
 
 
-export const FrontPage = () => {
+export class FrontPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.myRef = React.createRef()
+        this.state = { scrollTop: 0, arrowTriggered: false }
+    }
 
-    const [arrowTriggered, setArrowTriggered] = useState(false)
+    onScroll = () => {
+        const scrollTop = this.myRef.current.scrollTop
+        this.setState({
+            scrollTop: scrollTop
+        })
+    }
 
-    const arrowStyle = useSpring({
-        transform: arrowTriggered ? 'translate(0px, 4px)' : 'translate(0px, 0px)',
-        config: {
-            tension: 300,
-            friction: 1,
-        },
-    })
+    render() {
+        const {
+            scrollTop,
+            arrowTriggered
+        } = this.state
 
-    useEffect(() => {
-        if (!arrowTriggered) {
-          return
-        }
-      }, [arrowTriggered]);
+        //const [arrowTriggered, setArrowTriggered] = useState(false)
 
-    return (
-        <>
+        const arrowStyle = useSpring({
+            transform: arrowTriggered ? 'translate(0px, 4px)' : 'translate(0px, 0px)',
+            config: {
+                tension: 300,
+                friction: 1,
+            },
+        })
+
+        useEffect(() => {
+            if (!arrowTriggered) {
+                return
+            }
+        }, [arrowTriggered])
+
+        return (
+            <div 
+                className="frontpage-container"
+                ref={this.myRef}
+                onScroll={this.onScroll}
+            >
             <div className="sky-background">
                 <div 
                     className="constellations-wrapper"
-                    onMouseOver={() => setArrowTriggered(arrowTriggered ? false : true)}>
-                        <Constellations rotatePos={0}/>
-                        <animated.div 
+                    onMouseOver={() => {
+                        this.setState({
+                            arrowTriggered: !arrowTriggered
+                        })}}
+                >
+                        <Constellations rotatePos={scrollTop}/>
+                        <div 
                             className="intro"
                         >
                             <img src={require('../graphics/TRANSPARENT SPLASH GRAPHIC.png')} alt=""/>
-                        </animated.div>
+                        </div>
                         <animated.div className="scroll-indicator" style={arrowStyle}>
                             Scroll
                             <ArrowDown />
@@ -43,6 +70,8 @@ export const FrontPage = () => {
                 </div>
                 <TopBar />
             </div>
-        </>
-    )
+            <ExperimentalObj />
+        </div>
+        )
+    }
 }
