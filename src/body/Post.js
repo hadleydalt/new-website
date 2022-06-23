0078403efc703c5c1d0d2a08a2a46041f25e85d8
hadleydalt <hadley_dalton@brown.edu>
2022-06-23 15:26:10 +0200

    post animations removed

--- src/body/Post.js
+++ src/body/Post.js
@@ -1,80 +1,16 @@
 import '../App.css';
 import { Tag } from '../graphics/icons/Tag'
 import { PostLearnMore } from './PostLearnMore';
-import React, { useEffect, useState } from 'react';
-import {useSpring, animated} from 'react-spring'
-import { SamplePost } from './posts/SamplePost'
 
 export const Post = (props) => {
-
-    const [expanded, setExpanded] = useState(false)
 
     const tags = props.tags ? props.tags : ['Tag 1', 'Tag 2', 'Tag 3']
     const title = props.title ? props.title : 'Project Title'
     const date = props.date ? props.date : 'May 2020'
     const imgPath = props.imgPath ? props.imgPath : 'sample img'
-    const innerChild = props.innerChild ? props.innerChild : <SamplePost />
-    const postHeight = props.postHeight ? props.postHeight : '60vh'
-
-    const fileStyle = useSpring({
-        marginLeft: expanded ? '-150vw' : '0px',
-        opacity: expanded ? 0 : 1,
-        config: {
-            tension: 100,
-            friction: 15
-        }
-    })
-
-    const imgStyle = useSpring({
-        marginLeft: expanded ? '100%' : '7%',
-        opacity: expanded ? 0 : 1,
-        config: {
-            tension: 100,
-            friction: 15
-        }
-    })
-
-    const postDateStyle = useSpring({
-        marginTop: expanded ? '2.5vh' : '7vh',
-        config: {
-            tension: 100,
-            friction: 15
-        }
-    })
-
-    const postTitleWrapperStyle = useSpring({
-        marginTop: expanded ? '10vh' : '12vh',
-        config: {
-            tension: 100,
-            friction: 15
-        }
-    })
-
-    const postInnerBGStyle = useSpring({
-        marginTop: expanded ? '30%' : '100%',
-        opacity: expanded ? 1 : 0,
-        config: {
-            tension: 100, 
-            friction: 15
-        }
-    })
-
-    const postStyle = useSpring({
-        height: expanded ? postHeight : '50vh', 
-        config: {
-            tension: 100,
-            friction: 15
-        }
-    })
-
-    useEffect(() => {
-        if (!expanded) {
-            return
-        }
-    }, [expanded, 100])
 
     return (
-        <animated.div className="post" style={postStyle}>
+        <div className="post">
             <div className="post-tags-wrapper">
                 <Tag />
                 <div className="post-tags-text-wrapper">
@@ -89,25 +25,22 @@ export const Post = (props) => {
                 })}
                 </div>
             </div>
-            <animated.img src={require('../graphics/images/File.png')} alt="" style={fileStyle}/>
-            <animated.div className="post-title-wrapper" style={postTitleWrapperStyle}>
+            <img src={require('../graphics/images/File.png')} alt=""/>
+            <div className="post-title-wrapper">
                 <div className="post-title-icon" />
-                <div className="post-title-text" style={{color: expanded ? 'white' : '#909090'}}>{title}</div>
-            </animated.div>
-            <animated.div className="post-date-wrapper" style={postDateStyle}>
-                <div className="post-date-icon" style={{background: expanded ? 'transparent' : 'rgba(145,189,199,1)'}} />
-                <div className="post-date-text">Created in <date style={{color: expanded ? 'white' : 'rgba(145,189,199,1'}}>{date}</date></div>
-            </animated.div>
-            <animated.div className="post-image-wrapper" style={imgStyle}>
+                <div className="post-title-text">{title}</div>
+            </div>
+            <div className="post-date-wrapper">
+                <div className="post-date-icon" />
+                <div className="post-date-text">Created in <date>{date}</date></div>
+            </div>
+            <div className="post-image-wrapper">
                 <img src={require('../graphics/images/' + imgPath + '.png')} alt="" style={{width: '70%', height: '45%'}} />
                 <div class="post-image-tape" />
-            </animated.div>
-            <div onMouseDown={() => setExpanded(expanded ? false : true)}>
-                <PostLearnMore pressed={expanded}/>
             </div>
-            <animated.div className="post-inner-bg" style={postInnerBGStyle}>
-                {innerChild}
-            </animated.div>
-        </animated.div>
+            <div onMouseDown={() => alert("Hi")}>
+                <PostLearnMore />
+            </div>
+        </div>
     )
 }
--- src/body/PostLearnMore.js
+++ src/body/PostLearnMore.js
@@ -2,18 +2,17 @@ import '../App.css';
 import React, { useEffect, useState } from 'react';
 import {useSpring, animated} from 'react-spring'
 
-export const PostLearnMore = (props) => {
+export const PostLearnMore = () => {
 
     const [hovered, setHovered] = useState(false)
-    const pressed = props.pressed
     
     const style = useSpring({
         fontSize: hovered ? '2.2vh' : '2vh',
         fontWeight: hovered ? 'bold' : 'none',
         textDecoration: hovered ? 'underline' : 'none',
         letterSpacing: hovered ? '1px' : '0px',
-        marginTop: pressed ? '17vh' : pressed && hovered ? '17vh' : hovered ? '18.9vh' : '19vh',
-        color: pressed ? 'rgb(250,250,250)' : 'rgb(145,189,199)',
+        marginTop: hovered ? '18.9vh' : '19vh',
+        color: 'rgb(145,189,199)',
         config: {
             tension: 50,
             friction: 10
@@ -32,7 +31,7 @@ export const PostLearnMore = (props) => {
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
             style={style}>
-                {pressed ? '← Collapse' : 'Click to learn more →'}
-            </animated.div>
+                Click to learn more →
+        </animated.div>
     )
 }
