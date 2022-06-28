57a04949832753da4586c523d2669c205df0a6fa
hadleydalt <hadley_dalton@brown.edu>
2022-06-28 23:39:25 +0200

    dash vids

--- src/pages/swe-ux-content/Dash.js
+++ src/pages/swe-ux-content/Dash.js
@@ -35,11 +35,11 @@ I was challenged to code my own version of DASH using <b>TypeScript</b>, <b>Reac
 
 <p>Synthesizing my findings, I proceeded to make a wireframe sketch that <b>integrated the four goal functionalities</b> of DASH while also <b>addressing the pain points</b>. </p>
 
-<img src={require("../../graphics/images/dash_4.png")} style={{width: '40%', marginLeft: '30%'}}/>
+<img src={require("../../graphics/images/dash_4.png")} style={{width: '40%'}}/>
 
 <p>I then transferred the sketch to a prototype in Figma...</p>
 
-<img src={require("../../graphics/images/dash_5.png")} style={{width: '40%', marginLeft: '30%'}}/>
+<img src={require("../../graphics/images/dash_5.png")} style={{width: '40%'}}/>
 
 <p>The next step was to code the functional web app using React, TypeScript, and SCSS.</p>
 
@@ -55,6 +55,26 @@ I was challenged to code my own version of DASH using <b>TypeScript</b>, <b>Reac
 
 <img src={require("../../graphics/images/dash_7.png")} style={{width: '100%'}}/>
 <img src={require("../../graphics/images/dash_8.png")} style={{width: '100%'}}/>
+
+<p><h>Video Tours of the Result</h></p>
+
+<p><b><u>Create Notes, Move and Resize:</u></b> The user is able to populate the screen with "notes" onto which they can add text, images, videos, or support HTML iFrames (websites with clickable links).</p>
+
+<video autoPlay muted loop id="video" style={{width: '40%'}}>
+    <source src={require("../../graphics/images/dash_9.mov")} type="video/mp4" />
+</video>
+
+<p><b><u>Create Collections and Nested Collections:</u></b> The user can also create collections to store additional notes. Collections may be nested inside collections. </p>
+
+<video autoPlay muted loop id="video" style={{width: '40%'}}>
+    <source src={require("../../graphics/images/dash_10.mov")} type="video/mp4" />
+</video>
+
+<p><b><u>Linking Notes Together:</u></b> Finally, the user can link notes by creating and naming "Folders" which store up to three notes. If a note has been added to a folder, the user has the option to click on and access the other contents that have been stored within that folder. </p>
+
+<video autoPlay muted loop id="video" style={{width: '40%'}}>
+    <source src={require("../../graphics/images/dash_11.mov")} type="video/mp4" />
+</video>
 
         </div>
     )
